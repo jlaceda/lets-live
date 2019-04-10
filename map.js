5f13cc21e8ffe59e90f9c4963dca20aec6f49c9f
mrgmacandog <24902881+mrgmacandog@users.noreply.github.com>
2019-04-09 21:18:02 -0700

    Show current location

---
+++ map.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    let mapDisplay = L.map("map").setView([47.6062, -122.3321], 13);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1IjoibXJnbWFjYW5kb2ciLCJhIjoiY2plMGFydTNhMGZvZjJ3cXQxMXh0czV5ZSJ9.wLEGScCuILcHiezNwPpzaw'
+    }).addTo(mapDisplay);
+
+    function getLocation() {
+        navigator.geolocation.getCurrentPosition(function(location) {
+        
+            let latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
+
+            yourLocation = L.marker(latlng).addTo(mapDisplay);
+            console.log(latlng);
+
+            return [latlng.lat, latlng.lng];
+
+            // mymap.flyTo(latlng, 16);
+        });
+    }
+
+
+    getLocation();
+});
