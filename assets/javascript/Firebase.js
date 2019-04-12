bd045ef19bb98c6b16afc24bbb41238a783192c1
Alex Poplawski <alex0poplawski@gmail.com>
2019-04-11 18:30:44 -0700

    update functions and test html

--- assets/javascript/Firebase.js
+++ assets/javascript/Firebase.js
@@ -1,24 +1,48 @@
 "use strict";
-firebaseModule = {
+let firebaseModule = {
     config : 
     {
-    apiKey: "AIzaSyDlnKhpzpyOnPU0Qkp91SuIqjVLbJ6L37Q",
-    authDomain: "group-project-1-a401c.firebaseapp.com",
-    databaseURL: "https://group-project-1-a401c.firebaseio.com",
-    projectId: "group-project-1-a401c",
-    storageBucket: "",
-    messagingSenderId: "740996231944"
+        apiKey: "AIzaSyDlnKhpzpyOnPU0Qkp91SuIqjVLbJ6L37Q",
+        authDomain: "group-project-1-a401c.firebaseapp.com",
+        databaseURL: "https://group-project-1-a401c.firebaseio.com",
+        projectId: "group-project-1-a401c",
+        storageBucket: "",
+        messagingSenderId: "740996231944"
     },
     database: null,
     playersRef: null,
     chatRef: null,
-    init : function()
+    lastSearch: 0,
+    lastUID: 0,
+    
+
+    init: function()
     {
-        firebase.initializeApp(config);
+        firebase.initializeApp(this.config);
         this.database = firebase.database();
         this.recentSearchRef = this.database.ref("/recentSearch");
-        this.UserIDRef = this.database.ref("/UserID");
+        this.userIDSRef = this.database.ref("/userIDS");
+        this.variablesRef = this.database.ref("/vars");
+        this.addListeners();
+    },
+
+    addListeners: function()
+    {
+        this.variablesRef.on("value", function (snapshot) {
+            firebaseModule.lastUID = snapshot.val().lastUID;
+            firebaseModule.lastSearch = snapshot.val().lastSearchNum;
+        }, firebaseModule.handleErrors);
+
+        
+    },
+
+    updateVars: function()
+    {
+        this.variablesRef.update({lastUID: this.lastUID, lastSearchNum: this.lastSearch});
     }
     
 }
 firebaseModule.init();
+
+//testing
+firebaseModule.updateVars();
